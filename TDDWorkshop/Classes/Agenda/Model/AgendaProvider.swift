f3e4f5a7c7e8236a54e0e00d0fb0772ace3361ab
Aleksander Zubala <azubala@applause.com>
2015-11-21 09:40:48 +0100

    Updated agenda.

--- TDDWorkshop/Classes/Agenda/Model/AgendaProvider.swift
+++ TDDWorkshop/Classes/Agenda/Model/AgendaProvider.swift
@@ -17,13 +17,13 @@ class AgendaProvider: AgendaProviding {
     func reloadAgenda(completion: () -> ()) {
         dispatch_async(dispatch_get_global_queue(2, 0)) {
             self.agendaItems = [
-                    AgendaItem(time: "9:00", name: "Introduction"),
-                    AgendaItem(time: "11:00", name: "Coffee Break"),
-                    AgendaItem(time: "11:15", name: "Testing Techniques"),
-                    AgendaItem(time: "12:30", name: "Lunch"),
-                    AgendaItem(time: "13:30", name: "Red Green Refactor"),
-                    AgendaItem(time: "14:30", name: "Coffee Break"),
-                    AgendaItem(time: "15:00", name: "Legacy Code"),
+                    AgendaItem(time: "10:00", name: "Introduction"),
+                    AgendaItem(time: "11:30", name: "Coffee Break"),
+                    AgendaItem(time: "11:45", name: "Testing Techniques"),
+                    AgendaItem(time: "13:15", name: "Lunch"),
+                    AgendaItem(time: "14:15", name: "Red Green Refactor"),
+                    AgendaItem(time: "15:45", name: "Coffee Break"),
+                    AgendaItem(time: "16:00", name: "Legacy Code"),
             ]
 
             dispatch_async(dispatch_get_main_queue(), completion)
